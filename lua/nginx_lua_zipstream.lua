8d4b83971a62e6aceba9ff6a2cb2b645683687b7
Ben Timby <btimby@gmail.com>
2020-03-24 13:42:40 -0400

    Some logging, fix method for subrequest.

--- lua/nginx_lua_zipstream.lua
+++ lua/nginx_lua_zipstream.lua
@@ -10,6 +10,13 @@ local CHUNK_SIZE = ngx.var.chunk_size or 65535
 local ZIPNAME = ngx.var.zipname or 'multi.zip'
 local METHODS = ngx.var.methods or 'GET POST'
 
+-- For conversion of method name to method constant.
+local METHOD_MAP = {}
+METHOD_MAP['POST'] = ngx.HTTP_POST
+METHOD_MAP['GET'] = ngx.HTTP_GET
+METHOD_MAP['HEAD'] = ngx.HTTP_HEAD
+METHOD_MAP['PUT'] = ngx.HTTP_PUT
+
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- Library functions
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -19,12 +26,6 @@ local splitlines = function(str)
         table.insert(lines, s)
     end
     return lines
-end
-
-local unescape = function(url)
-    return url:gsub("%%(%x%x)", function(x)
-        return string.char(tonumber(x, 16))
-    end)
 end
 
 local make_reader = function(path)
@@ -52,16 +53,28 @@ end
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- Main routine.
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-local method = ngx.req.get_method()
-if not string.match(METHODS, method) then
+local method_name = ngx.req.get_method()
+if not string.match(METHODS, method_name) then
     -- We are not to handle this method.
     return ngx.exec(UPSTREAM)
 end
 
+-- Convert method name returned by ngx.req.get_method() to the constant needed
+-- by ngx.location.capture().
+local method_id = METHOD_MAP[method_name]
+if (method_id == nil) then
+    ngx.log(ngx.ERR, 'Unsupported method ' .. method_name .. ', add to METHOD_MAP')
+    ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+end
+
+-- Prepare for subrequest, we must have read the body in order for the body to
+-- be forwarded in the subrequest.
+ngx.req.read_body()
+
 -- Do a subrequest to origin. This will proxy to upstream, but allow us to
 -- ispect the response. Ensure the method matches parent request, in which
 -- case the body (if a POST or PUT) will be forwarded.
-local r = ngx.location.capture(UPSTREAM, { method = method })
+local r = ngx.location.capture(UPSTREAM, { method = method_id })
 
 -- Get magic header.
 local archive = r.header[HEADER_NAME]
@@ -70,6 +83,8 @@ local archive = r.header[HEADER_NAME]
 -- support other archive formats in the future.
 -- TODO: maybe return an error?
 if archive ~= 'zip' then
+    ngx.log(ngx.WARN, 'Unsupported header ' .. HEADER_NAME .. ': ' .. archive)
+
     -- Copy headers and status from subrequest.
     ngx.headers = r.header
     ngx.status = r.status
@@ -95,7 +110,7 @@ end)
 for _, entry in pairs(splitlines(r.body)) do
     -- Parse each line, format: crc32 size uri name
     local _, _, uri, name = string.match(entry, "(.-)%s(.-)%s(.-)%s(.*)")
-    local path = unescape(uri)
+    local path = ngx.unescape_uri(uri)
 
     ZipStream:write(name, make_reader(path))
 end
